700ce1e173fc2982dd14a7faf12341c7aaf03b4b
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-14 22:31:27 +0000

    Ex. 1 - ChangeString, versão final

---
+++ ChangeString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Say something: ");
+            string sentence = Console.ReadLine();
+            Console.WriteLine("Insert a character: ");
+            char character = char.Parse(Console.ReadLine());
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (character != sentence[i])
+                {
+                    Console.Write(sentence[i]);
+                }
+            }
+        }
+    }
+}
