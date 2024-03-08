afafdacfaf7141b61309837f131af573db9ec80a
AntonioLVieira <antonio.allvieira@gmail.com>
2024-03-08 20:40:09 +0000

    resolução exercicio 1 (RightSize)

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -6,7 +6,19 @@ namespace RightSize
     {
         static void Main(string[] args)
         {
-            
+            foreach (string i in args)
+            {
+                if (i.Length > 6)
+                {
+                    Console.WriteLine($"Argumento demasiado grande: {i}");
+                    return;
+                }
+
+                if (i.Length > 3)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
